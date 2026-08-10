37099a47a489b730d7b3e8ff9cd0705c75fb4c58
Aditya <aditjadhav27@gmail.com>
2026-08-10 22:01:53 +0530

    learn more concepts of list with harry

--- Basic py/chapter2.py
+++ Basic py/chapter2.py
@@ -1,3 +1,7 @@
 list=["friend","family","colleague", 20, 30, 40, 50, 2.43,4.5,True, False]
 print(list)
 print(list[2:8])
+list.insert(3, "neighbor")
+print(list)  #list is mutable and does not create a new list with the inserted value
+list.remove("friend")
+print("List after removing 'friend':", list)
