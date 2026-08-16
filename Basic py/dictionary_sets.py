5995c2a40518f02e7b2459128716e49a3c552e93
Aditya <aditjadhav27@gmail.com>
2026-08-16 21:45:37 +0530

    learn dic

---
+++ Basic py/dictionary_sets.py
@@ -0,0 +1,21 @@
+marks = {
+    "adi": 100,
+    "sujal": 60,
+    "sagar": 70,
+    "age1":20,
+    "age2":22
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+
+# marks["age2"]=30
+# print(marks.items())
+
+# print(marks["adi"])
+
+# print(marks.get("adii"))
+
+marks["AJ"] = 99
+print(marks.items())
