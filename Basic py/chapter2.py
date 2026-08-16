d53170bc0c32f93a50b3e9651a6648742884b145
Aditya <aditjadhav27@gmail.com>
2026-08-16 19:12:56 +0530

    updated

--- Basic py/chapter2.py
+++ Basic py/chapter2.py
@@ -10,7 +10,9 @@ print("Count of 'family' in the list:", list.count("family"))
 a=[1,2,3,4,5,45,60,34,23,12,11]
 b=["adit","jyoti","priya","shreya"]
 b.append("sneha")
-b.reverse(b)
+print(b)
+b.reverse()
+print(b)
 
 
 
