d0cb0d303ae5cc782b3e52d4f6b1190e3ba873c8
Aditya <aditjadhav27@gmail.com>
2026-08-19 23:00:50 +0530

    update

---
+++ Basic py/loops.py
@@ -0,0 +1,12 @@
+# num = int(input("ENter your no "))
+# sum =0
+# for i in range(1,num+1):
+#     sum+=i
+# print(sum)
+
+num = int(input("Enter your no "))
+product = 1
+for i in range(1,num+1):
+    product = product * i
+print(f"Factorial of {num} is {product}")
+
